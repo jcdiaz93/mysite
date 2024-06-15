c60f568b11eb226fd6d1578692ffcb072c186022
Jose <diazjosec93@gmail.com>
2024-06-15 02:23:54 -0300

    new commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-^_f@xpath5f-)+!vrxh6zkj)js@y%^=!(qgo!s67ztq67e@osy'
-SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-^_f@xpath5f-)+!vrxh6zkj)js@y%^=!(qgo!s67ztq67e@osy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
